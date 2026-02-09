6d45bb9327d892626aa56f8699ba9a46f2bac4cc
anup-kretoss <anup@kretoss.in>
2026-02-09 16:21:35 +0530

    remove localhost from vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ if (
   delete process.env.HOST;
 }
 
-const host = new URL(process.env.SHOPIFY_APP_URL || "http://localhost")
+const host = new URL(process.env.SHOPIFY_APP_URL || "")
   .hostname;
 
 let hmrConfig;
